3c103bc52a9a3520da142721c686ee17113cc5ff
xXNurioXx <xxnurioxx@gmail.com>
2019-11-17 23:52:36 +0100

    DefineRestMethods > Updated methods

--- src/main/java/me/nurio/webs/minecraftserverslist/hibernate/services/MinecraftServerService.java
+++ src/main/java/me/nurio/webs/minecraftserverslist/hibernate/services/MinecraftServerService.java
@@ -2,6 +2,8 @@ package me.nurio.webs.minecraftserverslist.hibernate.services;
 
 import me.nurio.webs.minecraftserverslist.hibernate.models.MinecraftServer;
 import me.nurio.webs.minecraftserverslist.hibernate.repositories.MinecraftServerRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,7 +22,17 @@ public class MinecraftServerService {
     }
 
     public List<MinecraftServer> findAll(){
-        return this.mcRepository.findAll();
+        return mcRepository.findAll();
+    }
+
+    public List<MinecraftServer> findAll(Pageable pageable){
+        Page<MinecraftServer> li = mcRepository.findAll(pageable);
+        return li.getContent();
+    }
+
+    public List<MinecraftServer> findAllOrderByServerScoreDesc(Pageable pageable){
+        Page<MinecraftServer> li = mcRepository.findAllByOrderByServerScoreDesc(pageable);
+        return li.getContent();
     }
 
 }
--- src/main/java/me/nurio/webs/minecraftserverslist/rest/ServerListRestController.java
+++ src/main/java/me/nurio/webs/minecraftserverslist/rest/ServerListRestController.java
@@ -8,6 +8,8 @@ import me.nurio.webs.minecraftserverslist.rest.responses.MinecraftPingResponse;
 import me.nurio.webs.minecraftserverslist.rest.responses.MinecraftServerDetailsResponse;
 import me.nurio.webs.minecraftserverslist.rest.responses.MinecraftServerResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,33 +19,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/servers")
-public class MinecraftServersRestController {
+@RequestMapping("/list")
+public class ServerListRestController {
 
     @Autowired(required = true)
     private MinecraftServerService serverService;
     @Autowired(required = true)
     private MinecraftPingService pingService;
 
-    @GetMapping("/{serverDomain:[\\w\\d-.]+}")
-    public MinecraftServerResponse getServer(@PathVariable String serverDomain) {
-        MinecraftServer mcServer = serverService.findByServerDomain(serverDomain);
-        return new MinecraftServerResponse(mcServer);
+    @GetMapping("")
+    public List<MinecraftServerResponse> getServer() {
+        List<MinecraftServer> mcServers = serverService.findAll();
+
+        List<MinecraftServerResponse> serverList = new ArrayList<>();
+        mcServers.forEach((mcServer) -> serverList.add(new MinecraftServerResponse(mcServer)));
+        return serverList;
     }
 
-    @GetMapping("/{serverDomain:[\\w\\d-.]+}/statistics")
-    public List<MinecraftPingResponse> getServerPings(@PathVariable String serverDomain) {
-        MinecraftServer mcServer = serverService.findByServerDomain(serverDomain);
-        List<MinecraftPing> mcPingList = pingService.findAllByServerId(mcServer.getServerId());
+    @GetMapping("/{page:[\\d]+}")
+    public List<MinecraftServerResponse> getServerPings(@PathVariable Integer page) {
+        Pageable pageable = PageRequest.of(page - 1, 25);
+        List<MinecraftServer> mcServers = serverService.findAllOrderByServerScoreDesc(pageable);
 
-        List<MinecraftPingResponse> pingList = new ArrayList<>();
-        mcPingList.forEach((mcPing) -> pingList.add(new MinecraftPingResponse(mcPing)));
-        return pingList;
+        List<MinecraftServerResponse> serverList = new ArrayList<>();
+        mcServers.forEach((mcServer) -> serverList.add(new MinecraftServerResponse(mcServer)));
+        return serverList;
     }
 
-    @GetMapping("/{serverDomain:[\\w\\d-.]+}/details")
-    public MinecraftServerDetailsResponse getServerDetails(@PathVariable String serverDomain) {
-        MinecraftServer mcServer = serverService.findByServerDomain(serverDomain);
-        return new MinecraftServerDetailsResponse(mcServer);
-    }
 }
