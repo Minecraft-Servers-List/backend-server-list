364bb9bbac4442c4a72ce9bab5b9de9b9d019dc9
xXNurioXx <xxnurioxx@gmail.com>
2019-11-18 21:32:52 +0100

    DefineRestMethods > Added ID methods

--- src/main/java/me/nurio/webs/minecraftserverslist/rest/MinecraftServersRestController.java
+++ src/main/java/me/nurio/webs/minecraftserverslist/rest/MinecraftServersRestController.java
@@ -31,10 +31,25 @@ public class MinecraftServersRestController {
         return new MinecraftServerResponse(mcServer);
     }
 
+    @GetMapping("/{serverId:[\\d-.]+}")
+    public MinecraftServerResponse getServer(@PathVariable Integer serverId) {
+        MinecraftServer mcServer = serverService.findByServerId(serverId);
+        return new MinecraftServerResponse(mcServer);
+    }
+
     @GetMapping("/{serverDomain:[\\w\\d-.]+}/statistics")
     public List<MinecraftPingResponse> getServerPings(@PathVariable String serverDomain) {
         MinecraftServer mcServer = serverService.findByServerDomain(serverDomain);
         List<MinecraftPing> mcPingList = pingService.findAllByServerId(mcServer.getServerId());
+
+        List<MinecraftPingResponse> pingList = new ArrayList<>();
+        mcPingList.forEach((mcPing) -> pingList.add(new MinecraftPingResponse(mcPing)));
+        return pingList;
+    }
+
+    @GetMapping("/{serverDomain:[\\w\\d-.]+}/statistics")
+    public List<MinecraftPingResponse> getServerPings(@PathVariable Integer serverId) {
+        List<MinecraftPing> mcPingList = pingService.findAllByServerId(serverId);
 
         List<MinecraftPingResponse> pingList = new ArrayList<>();
         mcPingList.forEach((mcPing) -> pingList.add(new MinecraftPingResponse(mcPing)));
@@ -46,4 +61,11 @@ public class MinecraftServersRestController {
         MinecraftServer mcServer = serverService.findByServerDomain(serverDomain);
         return new MinecraftServerDetailsResponse(mcServer);
     }
+
+    @GetMapping("/{serverDomain:[\\w\\d-.]+}/details")
+    public MinecraftServerDetailsResponse getServerDetails(@PathVariable Integer serverId) {
+        MinecraftServer mcServer = serverService.findByServerId(serverId);
+        return new MinecraftServerDetailsResponse(mcServer);
+    }
+
 }
